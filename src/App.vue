f3ccd8185d8a5590e2febba7658301262c262dd3
Ali Afthab <aliallu3xa@gmail.com>
2023-10-31 17:04:35 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1
+  class="text-3xl font-bold underline text-green-400 bg-slate-200 text-center h-20"
+  >
+  Hello world!
+</h1>
+<RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main class="text-center">ketherhj</main>
+</template>
